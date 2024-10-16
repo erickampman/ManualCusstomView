dfb430c2457059e0c4aa9c73c33885f5b59ceb2c
Eric Kampman <erickampman@yahoo.com>
2024-10-16 07:19:46 -0700

    Initial Commit

---
+++ ManualCusstomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualCusstomView
+//
+//  Created by Eric Kampman on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
